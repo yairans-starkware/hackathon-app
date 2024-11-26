c2ac37bc6eb66bde337f952167d1c2cfc75d7ca3
madmonl-starkware <liad@starkware.co>
2024-11-26 23:07:39 +0200

    Add src prefix to no meals illustration

--- apps/foodiez/src/components/StatsCard/EmptyStatsCard.tsx
+++ apps/foodiez/src/components/StatsCard/EmptyStatsCard.tsx
@@ -1,4 +1,5 @@
 import { AppTabs } from "../../types/ui";
+import { SrcPrefix } from "../../utils/consts";
 import { Button } from "../ui/button"
 import { Card, CardContent } from "../ui/card"
 
@@ -14,7 +15,7 @@ export const EmptyStatsCard = ({
   return (
     <Card className="p-6 text-center">
       <CardContent className="flex flex-col items-center">
-        <img src={`/empty-meals-illustration.webp`} alt='no-meals-illustration' className="h-56 w-56 rounded-xl"/>
+        <img src={`${SrcPrefix}/empty-meals-illustration.webp`} alt='no-meals-illustration' className="h-56 w-56 rounded-xl"/>
         <h3 className="text-xl font-semibold mt-4 mb-2">No meals found</h3>
         <p className="text-gray-600 max-w-md">
           {`You haven\'t participated in any meals in the selected month. Start your culinary journey by registering for an upcoming meal!`}
